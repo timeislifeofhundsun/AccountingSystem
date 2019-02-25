e59297887d6df61a17bf1f9d04cabd9bbcb7588d
gkwang <370377342@qq.com>
2019-02-25 17:21:51 +0800

    修改static目录

--- src/main/resources/static/admin.js
+++ src/main/resources/static/admin.js
@@ -1,10 +1,10 @@
 angular.module('app',['ngRoute'])
 .config(['$routeProvider', function($routeProvider){
     $routeProvider
-    .when('/',{templateUrl:'view/dashboard.html'})
+    .when('/',{templateUrl: 'views/dashboard.html'})
     .when('/accounting',{template:'账套页面'})
     .when('/printers',{template:'这是打印机页面'})
-    .when('/voucher',{templateUrl:'view/voucher.html'})
+    .when('/voucher',{templateUrl:'views/voucher.html'})
     .otherwise({redirectTo:'/'});
 }]);
 
