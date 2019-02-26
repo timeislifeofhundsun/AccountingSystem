3d0123ef6e2f1cabc56a8e6ae98995cf66b1d0e6
yangjf25257 <1205882008@qq.com>
2019-02-26 16:41:58 +0800

    IDEA

---
+++ src/main/java/com/hundsun/accountingsystem/yang/controller/Test.java
@@ -0,0 +1,11 @@
+package com.hundsun.accountingsystem.yang.controller;
+
+/**
+ * @ClassName Test
+ * @Aurhor yangjf25257
+ * @Date 2019/2/26 16:41
+ * @Version 1.0
+ * @Description
+ **/
+public class Test {
+}
