68628678c8d50d835790d96e167a72750c2c2a67
yangjf25257 <1205882008@qq.com>
2019-02-26 16:21:16 +0800

    IDEA

---
+++ src/main/java/com/hundsun/accountingsystem/yang/service/Hello.java
@@ -0,0 +1,11 @@
+package com.hundsun.accountingsystem.yang.service;
+
+/**
+ * @ClassName Hello
+ * @Aurhor yangjf25257
+ * @Date 2019/2/26 16:19
+ * @Version 1.0
+ * @Description
+ **/
+public class Hello {
+}
