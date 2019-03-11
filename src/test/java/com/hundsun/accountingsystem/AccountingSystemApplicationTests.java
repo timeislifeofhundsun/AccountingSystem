fe6e76466a03d45aa57912cd2441a42144aa962d
yangjf25257 <1205882008@qq.com>
2019-03-11 16:08:40 +0800

    过户库添加List数组

--- src/test/java/com/hundsun/accountingsystem/AccountingSystemApplicationTests.java
+++ src/test/java/com/hundsun/accountingsystem/AccountingSystemApplicationTests.java
@@ -1,17 +1,36 @@
 package com.hundsun.accountingsystem;
 
 
+import com.hundsun.accountingsystem.Global.bean.GHK;
+import com.hundsun.accountingsystem.Global.mapper.GHKMapper;
+import com.hundsun.accountingsystem.Global.util.FileParsing;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AccountingSystemApplicationTests {
 
+	@Autowired
+	GHKMapper ghkMapper;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws IOException {
+		FileParsing fileParsing = new FileParsing();
+		List<GHK> list= fileParsing.ReadDbf("C:\\Users\\yangjf25257\\Desktop\\JYQS\\GH32562.dbf");
+//		int i = 1;
+//		for (GHK ghk : list){
+//			ghkMapper.insert(ghk);
+//			System.out.println(i);
+//			i ++;
+//		}
+		ghkMapper.insert_list(list);
 	}
 
 }
