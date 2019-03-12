4abb0099e7c81e3e8655146c84a91f3d44fb7689
yangjf25257 <1205882008@qq.com>
2019-03-12 11:13:49 +0800

    更新

--- src/main/java/com/hundsun/accountingsystem/Global/bean/SysUser.java
+++ src/main/java/com/hundsun/accountingsystem/Global/bean/SysUser.java
@@ -1,10 +1,18 @@
 package com.hundsun.accountingsystem.Global.bean;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.Accessors;
 
 import java.util.List;
 
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Accessors(chain = true)
 public class SysUser {
     private Integer uid;
     private String username;
