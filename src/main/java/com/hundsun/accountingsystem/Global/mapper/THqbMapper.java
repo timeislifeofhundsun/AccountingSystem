523e5a6657de1d300a9ab2ccfa115717b8de8d3c
gaozhen <gaozhen3h@163.com>
2019-03-12 16:29:38 +0800

    1.配置log4j 2.读取行情文件完成
    Signed-off-by: gaozhen <gaozhen3h@163.com>

--- src/main/java/com/hundsun/accountingsystem/Global/mapper/THqbMapper.java
+++ src/main/java/com/hundsun/accountingsystem/Global/mapper/THqbMapper.java
@@ -1,17 +1,90 @@
 package com.hundsun.accountingsystem.Global.mapper;
+import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
+import com.hundsun.accountingsystem.Global.bean.Assist;
 import com.hundsun.accountingsystem.Global.bean.THqb;
 
-public interface THqbMapper {
-    int deleteByPrimaryKey(Integer zqnm);
-
-    int insert(THqb record);
-
-    int insertSelective(THqb record);
-
-    THqb selectByPrimaryKey(Integer zqnm);
-
-    int updateByPrimaryKeySelective(THqb record);
-
-    int updateByPrimaryKey(THqb record);
+public interface THqbMapper{
+	/**
+	 * 获得THqb数据的总行数,可以通过辅助工具Assist进行条件查询,如果没有条件则传入null
+	 * @param assist
+	 * @return
+	 */
+    long getTHqbRowCount(Assist assist);
+	/**
+	 * 获得THqb数据集合,可以通过辅助工具Assist进行条件查询,如果没有条件则传入null
+	 * @param assist
+	 * @return
+	 */
+    List<THqb> selectTHqb(Assist assist);
+	/**
+	 * 获得一个THqb对象,以参数THqb对象中不为空的属性作为条件进行查询
+	 * @param obj
+	 * @return
+	 */
+    THqb selectTHqbByObj(THqb obj);
+	/**
+	 * 通过THqb的id获得THqb对象
+	 * @param id
+	 * @return
+	 */
+    THqb selectTHqbById(Integer id);
+	/**
+	 * 插入THqb到数据库,包括null值
+	 * @param value
+	 * @return
+	 */
+    int insertTHqb(THqb value);
+	/**
+	 * 插入THqb中属性值不为null的数据到数据库
+	 * @param value
+	 * @return
+	 */
+    int insertNonEmptyTHqb(THqb value);
+	/**
+	 * 批量插入THqb到数据库,包括null值
+	 * @param value
+	 * @return
+	 */
+    int insertTHqbByBatch(List<THqb> value);
+	/**
+	 * 通过THqb的id删除THqb
+	 * @param id
+	 * @return
+	 */
+    int deleteTHqbById(Integer id);
+	/**
+	 * 通过辅助工具Assist的条件删除THqb
+	 * @param assist
+	 * @return
+	 */
+    int deleteTHqb(Assist assist);
+	/**
+	 * 通过THqb的id更新THqb中的数据,包括null值
+	 * @param enti
+	 * @return
+	 */
+    int updateTHqbById(THqb enti);
+ 	/**
+	 * 通过辅助工具Assist的条件更新THqb中的数据,包括null值
+	 * @param value
+	 * @param assist
+	 * @return
+	 */
+    int updateTHqb(@Param("enti") THqb value, @Param("assist") Assist assist);
+	/**
+	 * 通过THqb的id更新THqb中属性不为null的数据
+	 * @param enti
+	 * @return
+	 */
+    int updateNonEmptyTHqbById(THqb enti);
+ 	/**
+	 * 通过辅助工具Assist的条件更新THqb中属性不为null的数据
+	 * @param value
+	 * @param assist
+	 * @return
+	 */
+    int updateNonEmptyTHqb(@Param("enti") THqb value, @Param("assist") Assist assist);
 }
--- src/test/java/com/hundsun/accountingsystem/AccountingSystemApplicationTests.java
+++ src/test/java/com/hundsun/accountingsystem/AccountingSystemApplicationTests.java
@@ -1,21 +1,16 @@
 package com.hundsun.accountingsystem;
 
 
-import com.hundsun.accountingsystem.Global.bean.TGhk;
-import com.hundsun.accountingsystem.Global.bean.TJyfl;
-import com.hundsun.accountingsystem.Global.bean.TKjkmb;
-import com.hundsun.accountingsystem.Global.mapper.TGhkMapper;
-import com.hundsun.accountingsystem.Global.mapper.TJyflMapper;
-import com.hundsun.accountingsystem.Global.mapper.TKjkmbMapper;
-import com.hundsun.accountingsystem.Global.util.FileParsing;
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.io.IOException;
-import java.util.List;
+import com.hundsun.accountingsystem.Global.mapper.TGhkMapper;
+import com.hundsun.accountingsystem.Global.mapper.TJyflMapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,28 +20,10 @@ public class AccountingSystemApplicationTests {
 	TGhkMapper tGhkMapper;
 	@Autowired
 	TJyflMapper tJyflMapper;
+	
 
 	@Test
 	public void contextLoads() throws IOException {
-		/*FileParsing fileParsing = new FileParsing();
-		List<TGhk> list= fileParsing.ReadDbf("C:\\Users\\wanggk23608\\Desktop\\GH32562.dbf");
-//		int i = 1;
-//		for (TGhk ghk : list){
-//			ghkMapper.insert(ghk);
-//			System.out.println(i);
-//			i ++;
-//		}
-		tGhkMapper.insert_list(list);*/
-		/*List<TKjkmb> allKJKM = tKjkmbMapper.findAllKJKM();
-
-		for (TKjkmb tkjkmb:allKJKM
-			 ) {
-			System.out.println(tkjkmb.toString());
-		}*/
-		/*TJyfl tJyfl = new TJyfl();
-		tJyfl.setGh(12.0).setJsfl(23.6).setYh(45.23).setYj(456.32).setYwcode(12314).setYwname("dwadwa").setZg(1646.32);
-		TJyfl tJyfl1 = tJyflMapper.selectByPrimaryKey(12314);
-		System.out.println(tJyfl1.toString());*/
 	}
 
 }
