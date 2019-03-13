5b0aba686ed34c242be0a66495298fcb188199ef
gaozhen <gaozhen3h@163.com>
2019-03-13 16:34:20 +0800

    还原数据库ip
    Signed-off-by: gaozhen <gaozhen3h@163.com>

--- src/main/java/com/hundsun/accountingsystem/Global/service/serviceImpl/RZQSServiceImpl.java
+++ src/main/java/com/hundsun/accountingsystem/Global/service/serviceImpl/RZQSServiceImpl.java
@@ -1,14 +1,26 @@
 package com.hundsun.accountingsystem.Global.service.serviceImpl;
 
 import java.util.Date;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.hundsun.accountingsystem.Global.service.RZQSService;
+import com.hundsun.accountingsystem.Global.service.TGhkService;
+import com.hundsun.accountingsystem.Global.service.THqbService;
+import com.hundsun.accountingsystem.Global.util.DateFormatUtil;
+import com.hundsun.accountingsystem.Global.util.FilePathUtil;
 
 @Service
 public class RZQSServiceImpl implements RZQSService {
 
+	@Autowired
+	private THqbService tHqbService;
+	
+	@Autowired
+	private TGhkService tGhkService;
+	
 	/**
 	* @Description: 日终清算
 	* 1.读取交易数据
@@ -22,17 +34,37 @@ public class RZQSServiceImpl implements RZQSService {
 	* @return boolean    返回类型
 	* 
 	* @author gaozhen
+	 * @throws Exception 
 	 */
 	@Override
-	public boolean rzqs(int ztbh, Date ywrq) {
-		boolean res =  false;
+	public boolean rzqs(int ztbh, Date ywrq) throws Exception {
+		boolean returnData =  false;
+		Map<String,String> files = FilePathUtil.
+				getFilePathByDate(DateFormatUtil.getStringByDate(ywrq));
+		String cash = files.get("cashsecurityclosemd");
+		String mkt = files.get("mktdt00");
+		String sjsmx =files.get("SJSMX1");
+		String gh = files.get("GH32562");
+		if(cash==null||mkt==null||sjsmx==null||gh==null) {
+			throw new Exception("接口文件不全");
+		}
 		/**
 		 * 1.读取交易数据
 		 */
+		boolean res = tGhkService.readGhDataByFile(gh,sjsmx,DateFormatUtil.getStringByDate(ywrq));
+		if(!res) {
+			throw new Exception("读取交易文件失败");
+		}
 		
+		/**
+		 * 2.读取行情数据
+		 */
+		res = tHqbService.readHqDataByFile(mkt, cash, ywrq);
+		if(!res) {
+			throw new Exception("读取行情文件失败");
+		}
 		
-		
-		return res;
+		return returnData;
 	}
 
 }
