786826a62222d37185cc9eb9cb41201cc248cb8f
gaozhen <gaozhen3h@163.com>
2019-03-14 10:06:04 +0800

    替换逆向工程过户库
    Signed-off-by: gaozhen <gaozhen3h@163.com>

--- src/main/java/com/hundsun/accountingsystem/Global/bean/TGhk.java
+++ src/main/java/com/hundsun/accountingsystem/Global/bean/TGhk.java
@@ -1,17 +1,13 @@
 package com.hundsun.accountingsystem.Global.bean;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
-import java.util.Date;
 /**
  * @ClassName TGhk
- * @Aurhor yangjf25257
  * @Date 2019/3/8 15:34
  * @Version 1.0
  * @Description 属性：业务流水号 股东代码 股东姓名 席位代码 账套编号 成交数量 成交价格 成交金额 证券代码 买卖 保存时间 交收时间 成交时间 结算账号
@@ -23,37 +19,23 @@ import java.util.Date;
 @AllArgsConstructor
 @Data
 @Accessors(chain = true)
-public class TGhk {
-    private Integer ywlsid;
-
-    private String gdcode;
-
-    private String gdname;
-
-    private String xwcode;
-
-    private Integer ztcode;
-
-    private Integer cjsl;
-
-    private Double cjje;
-
-    private Double cjjg;
-
-    private String zqcode;
-
-    private String bs;
-
-    private Date bctime;
-
-    private Date jstime;
-
-    private Date cjtime;
-
-    private String jszh;
-
-    private String bfjzh;
-
-    private Integer sclb;
+public class TGhk implements java.io.Serializable {
+    private static final long serialVersionUID = 1L;
+    private Integer ywlsid;//业务流水id
+    private String bfjzh;//备付金账号
+    private String jszh;//结算账号
+    private Integer ztcode;//账套编号
+    private Double cjjg;//成交价格
+    private java.util.Date bctime;//保存时间
+    private Double cjje;//成交金额
+    private String xwcode;//席位代码
+    private Integer cjsl;//成交数量
+    private java.util.Date cjtime;//成交时间
+    private String bs;//买卖方向
+    private String zqcode;//证券代码
+    private Integer sclb;//市场类别
+    private String gdcode;//股东代码
+    private java.util.Date jstime;//交收时间
+    private String gdname;//股东姓名
 
 }
--- src/main/java/com/hundsun/accountingsystem/Global/mapper/TGhkMapper.java
+++ src/main/java/com/hundsun/accountingsystem/Global/mapper/TGhkMapper.java
@@ -1,21 +1,87 @@
 package com.hundsun.accountingsystem.Global.mapper;
-
 import com.hundsun.accountingsystem.Global.bean.TGhk;
-
 import java.util.List;
-
-public interface TGhkMapper {
-    int deleteByPrimaryKey(Integer ywlsid);
-
-    int insert(TGhk record);
-
-    int insertSelective(TGhk record);
-
-    TGhk selectByPrimaryKey(Integer ywlsid);
-
-    int updateByPrimaryKeySelective(TGhk record);
-
-    int updateByPrimaryKey(TGhk record);
-
-    int insert_list(List<TGhk> list);
+import com.hundsun.accountingsystem.Global.bean.Assist;
+import org.apache.ibatis.annotations.Param;
+public interface TGhkMapper{
+	/**
+	 * 获得TGhk数据的总行数,可以通过辅助工具Assist进行条件查询,如果没有条件则传入null
+	 * @param assist
+	 * @return
+	 */
+    long getTGhkRowCount(Assist assist);
+	/**
+	 * 获得TGhk数据集合,可以通过辅助工具Assist进行条件查询,如果没有条件则传入null
+	 * @param assist
+	 * @return
+	 */
+    List<TGhk> selectTGhk(Assist assist);
+	/**
+	 * 获得一个TGhk对象,以参数TGhk对象中不为空的属性作为条件进行查询
+	 * @param obj
+	 * @return
+	 */
+    TGhk selectTGhkByObj(TGhk obj);
+	/**
+	 * 通过TGhk的id获得TGhk对象
+	 * @param id
+	 * @return
+	 */
+    TGhk selectTGhkById(Integer id);
+	/**
+	 * 插入TGhk到数据库,包括null值
+	 * @param value
+	 * @return
+	 */
+    int insertTGhk(TGhk value);
+	/**
+	 * 插入TGhk中属性值不为null的数据到数据库
+	 * @param value
+	 * @return
+	 */
+    int insertNonEmptyTGhk(TGhk value);
+	/**
+	 * 批量插入TGhk到数据库,包括null值
+	 * @param value
+	 * @return
+	 */
+    int insertTGhkByBatch(List<TGhk> value);
+	/**
+	 * 通过TGhk的id删除TGhk
+	 * @param id
+	 * @return
+	 */
+    int deleteTGhkById(Integer id);
+	/**
+	 * 通过辅助工具Assist的条件删除TGhk
+	 * @param assist
+	 * @return
+	 */
+    int deleteTGhk(Assist assist);
+	/**
+	 * 通过TGhk的id更新TGhk中的数据,包括null值
+	 * @param enti
+	 * @return
+	 */
+    int updateTGhkById(TGhk enti);
+ 	/**
+	 * 通过辅助工具Assist的条件更新TGhk中的数据,包括null值
+	 * @param value
+	 * @param assist
+	 * @return
+	 */
+    int updateTGhk(@Param("enti") TGhk value, @Param("assist") Assist assist);
+	/**
+	 * 通过TGhk的id更新TGhk中属性不为null的数据
+	 * @param enti
+	 * @return
+	 */
+    int updateNonEmptyTGhkById(TGhk enti);
+ 	/**
+	 * 通过辅助工具Assist的条件更新TGhk中属性不为null的数据
+	 * @param value
+	 * @param assist
+	 * @return
+	 */
+    int updateNonEmptyTGhk(@Param("enti") TGhk value, @Param("assist") Assist assist);
 }
