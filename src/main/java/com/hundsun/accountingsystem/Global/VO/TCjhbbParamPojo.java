d5083b5acd1669d98e45501b73616e93b8d72ddb
fzh86215921 <905770446@qq.com>
2019-03-19 14:46:42 +0800

    成交回报库查询条件pojo提交

---
+++ src/main/java/com/hundsun/accountingsystem/Global/VO/TCjhbbParamPojo.java
@@ -0,0 +1,19 @@
+package com.hundsun.accountingsystem.Global.VO;
+
+public class TCjhbbParamPojo {
+
+	private String startDate;
+	private String endDate;
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+}
