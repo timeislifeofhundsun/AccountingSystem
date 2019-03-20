fdefeae911dc5b9b33c82c79fd679467e3779831
gaozhen <gaozhen3h@163.com>
2019-03-20 10:50:12 +0800

    分笔计算经手费，佣金等，未插入到数据库，未测试
    Signed-off-by: gaozhen <gaozhen3h@163.com>

--- src/main/java/com/hundsun/accountingsystem/TGp/service/impl/GPQSServiceImpl.java
+++ src/main/java/com/hundsun/accountingsystem/TGp/service/impl/GPQSServiceImpl.java
@@ -1,7 +1,10 @@
 package com.hundsun.accountingsystem.TGp.service.impl;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -9,6 +12,7 @@ import com.hundsun.accountingsystem.Global.bean.TCjhbb;
 import com.hundsun.accountingsystem.Global.bean.TCjhbbExample;
 import com.hundsun.accountingsystem.Global.bean.TCjhbbExample.Criteria;
 import com.hundsun.accountingsystem.Global.bean.TJyfl;
+import com.hundsun.accountingsystem.Global.bean.TQsb;
 import com.hundsun.accountingsystem.Global.mapper.TCjhbbMapper;
 import com.hundsun.accountingsystem.Global.mapper.TJyflMapper;
 import com.hundsun.accountingsystem.TGp.service.GPQSService;
@@ -82,15 +86,63 @@ public class GPQSServiceImpl implements GPQSService{
 		criteria.andYwlbEqualTo(1);
 		criteria.andYwrqBetween(ywrq, ywrq);
 		List<TCjhbb> tCjhbbs = tCjhbbMapper.selectByExample(example);
+		/**
+		 * 根据证券类别分类
+		 */
+		Map<String, List<TCjhbb>> zqdmMap = new HashMap<>();
+		for (TCjhbb tCjhbb : tCjhbbs) {
+			String zqdm = tCjhbb.getZqdm();
+			List<TCjhbb> tcjs = zqdmMap.get(zqdm);
+			if(tcjs==null) {
+				tcjs = new ArrayList<>();
+			}
+			tcjs.add(tCjhbb);
+			zqdmMap.put(zqdm,tcjs);
+		}
 		
-		
-		returnData = false;
+		returnData = true;
 		return returnData;
 	}
 	
-	private boolean gpmr(TCjhbb tCjhbb) {
+	/**
+	 * 
+	* @Description: 计算经手费、印花税 
+	* @param  参数说明
+	* @return boolean    返回类型
+	* @author gaozhen
+	 */
+	private boolean gpmr(List<TCjhbb> tCjhbbs) {
 		boolean res = false;
-		
+		//校验参数
+		if(tCjhbbs.size()>0 && tCjhbbs.get(0).getMmfx().equals("B")) {
+			double cjje = 0;
+			double jsf = 0;
+			double ghf = 0;
+			double zgf= 0;
+			double jyfy = 0;
+			Integer cjsl = 0;
+			for (TCjhbb tCjhbb : tCjhbbs) {
+				double tempCjje = tCjhbb.getCjjg()*tCjhbb.getCjsl();
+				cjje=cjje+tempCjje;
+				jsf=jsf+tempCjje*this.mrjyfl.getJsfl();
+				ghf=ghf+tempCjje*this.mcjyfl.getGh();
+				zgf=zgf+tempCjje*this.mcjyfl.getZg();
+				jyfy=jyfy+tempCjje*this.mrjyfl.getYj();
+				cjsl=cjsl+tCjhbb.getCjsl();
+			}
+			double cyj = jyfy-jsf-zgf;
+			double zqqsk = cjje+jyfy-cyj;
+			TCjhbb tempTCjhbb = tCjhbbs.get(0);
+			TQsb qsb = new TQsb(null, tempTCjhbb.getZtbh(), tempTCjhbb.getYwrq(), 
+					tempTCjhbb.getZqdm(),1101, //ywlb
+					"B", cjsl, cjje, null, //印花税
+					jsf, ghf, zgf, cyj, null, //费用合计
+					null,null, null, null, null, null,//扩展字段
+					null, //公允价值变动
+					zqqsk,null//差价收入
+					, tempTCjhbb.getJysc(), cjje);//成本
+			
+		}
 		
 		res = true;
 		return res;
