01ae5948adfe8ecd46a7ea639e3e7261cc725e39
fzh86215921 <905770446@qq.com>
2019-03-22 09:48:41 +0800

    新增两费计息表

---
+++ src/main/java/com/hundsun/accountingsystem/Global/mapper/TLfjxbMapper.java
@@ -0,0 +1,30 @@
+package com.hundsun.accountingsystem.Global.mapper;
+
+import com.hundsun.accountingsystem.Global.bean.TLfjxb;
+import com.hundsun.accountingsystem.Global.bean.TLfjxbExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TLfjxbMapper {
+    int countByExample(TLfjxbExample example);
+
+    int deleteByExample(TLfjxbExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(TLfjxb record);
+
+    int insertSelective(TLfjxb record);
+
+    List<TLfjxb> selectByExample(TLfjxbExample example);
+
+    TLfjxb selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") TLfjxb record, @Param("example") TLfjxbExample example);
+
+    int updateByExample(@Param("record") TLfjxb record, @Param("example") TLfjxbExample example);
+
+    int updateByPrimaryKeySelective(TLfjxb record);
+
+    int updateByPrimaryKey(TLfjxb record);
+}
